15f1dbc1b5b11b666f1727fbd79c8c76dc28f4a8
Marco Nocerino <X00195027@mytudublin.ie>
2024-03-27 15:00:47 +0000

    add admin

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Project, Task
 
-# Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['project_id', 'name', 'project_team_size', 'project_start_date', 'project_end_date', 'empID']
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['task_id', 'project_id', 'task_name', 'task_description', 'time_required', 'task_completion', 'empID']
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Task, TaskAdmin)
