da049db62cb8e6ff224390a19fc4747197d4f7b4
Liam Mayenco Vela <X00204735@mytudublin.ie>
2024-03-27 14:49:39 +0000

    added chatgpt api

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from openai import OpenAI
+
+client = OpenAI(api_key='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')
+system_prompt = "As the project manager for our software development project, I need assistance in task assignment. Below are the project details, and time constraint. Please provide task assignments for the project requirements.:"
+user_prompt=""
+end_prompt = "Please provide in detail each task assignment for the team member and "
+final_prompt = system_prompt + user_prompt
+response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{
+            "role": "user",
+            "content": final_prompt
+        }],
+)
+
 
 # Create your views here.
