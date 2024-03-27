ebc652ca89594cdf868e681f00597705741cf558
Marco Nocerino <X00195027@mytudublin.ie>
2024-03-27 16:16:12 +0000

    added ai

--- projects/views.py
+++ projects/views.py
@@ -4,6 +4,26 @@ from django.views.decorators.http import require_http_methods
 from projects.models import Task
 from django.contrib.auth.decorators import login_required
 
+'''
+client = OpenAI(api_key='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')
+system_prompt = "As the project manager for our software development project, I need assistance in task assignment. Below are the project details, and time constraint. Please provide task assignments for the project requirements.:"
+end_prompt = "Provide an exhaustive list of tasks that need to be done in order to complete this project in the given time frame. The output apart from being in weeks have to be very precise and in details for each task so that they can be given to team members, I don't need you to split them to team members but I need you to make them very in detail so I can divide tasks for people. I also need them to be divided into each day of each week but each week has 5 working days."
+
+
+def task_assignment(request):
+    if request.method == 'POST':
+        user_prompt = request.POST['user_prompt']
+        final_prompt = system_prompt + user_prompt + end_prompt
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[{
+                "role": "user",
+                "content": final_prompt
+            }],
+        )
+        return render(request, 'task_assignment.html', {'response': response})
+'''
+
 @login_required
 def project_tasks(request, project_id):
     # Query the Task model to get all tasks for the given project ID
