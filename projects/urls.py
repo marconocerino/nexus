2d44a7b442d5d288aea8763054451d4c25f444d0
Neri Carcasci <nericarcasci@gmail.com>
2024-03-27 13:48:24 +0000

    calendar moved

--- projects/urls.py
+++ projects/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import monthly_tasks
 
 urlpatterns = [
-    path('monthly_tasks/', monthly_tasks, name='monthly_tasks'),
+    path('monthly_tasks/<int:course_id>', monthly_tasks, name='monthly_tasks'),
 ]
